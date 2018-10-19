4386670779be961583c8d84e600df268a0c6a0ad
Jorge Rodríguez-Araújo <grrodri@gmail.com>
2018-10-19 16:21:55 +0200

    Increase testing timeouts

--- test/abraiaTest.js
+++ test/abraiaTest.js
@@ -10,14 +10,14 @@ describe('Abraia', () => {
   it('upload a local file', async () => {
     const result = await abraia.fromFile(path.join(__dirname, '../images/lion.jpg'))
     assert(result.path.endsWith('lion.jpg'))
-  }).timeout(15000)
+  }).timeout(25000)
 
   // it('upload a remote file', () => {
   //   const url = 'https://abraia.me/images/random.jpg'
   //   return abraia
   //     .fromUrl(url)
   //     .then(data => assert(data.params.url === url))
-  // }).timeout(15000)
+  // }).timeout(25000)
 
   it('download stored file', async () => {
     const filename = path.join(__dirname, '../images/optimized.jpg')
@@ -25,7 +25,7 @@ describe('Abraia', () => {
       .fromFile(path.join(__dirname, '../images/lion.jpg'))
       .toFile(filename)
     assert(fs.lstatSync(filename).isFile())
-  }).timeout(20000)
+  }).timeout(25000)
 
   it('thumb resize', async () => {
     const filename = path.join(__dirname, '../images/roptim.jpg')
@@ -34,7 +34,7 @@ describe('Abraia', () => {
       .resize({ width: 500, height: 500, mode: 'thumb' })
       .toFile(filename)
     assert(fs.lstatSync(filename).isFile())
-  }).timeout(15000)
+  }).timeout(25000)
 
   it('image resize', async () => {
     const filename = path.join(__dirname, '../images/tiger_x333.jpg')
@@ -43,7 +43,7 @@ describe('Abraia', () => {
       .resize({ height: 333 })
       .toFile(filename)
     assert(fs.lstatSync(filename).isFile())
-  }).timeout(15000)
+  }).timeout(25000)
 
   it('smart resize', async () => {
     const filename = path.join(__dirname, '../images/tiger_333x333.jpg')
@@ -52,5 +52,5 @@ describe('Abraia', () => {
       .resize({ width: 333, height: 333 })
       .toFile(filename)
     assert(fs.lstatSync(filename).isFile())
-  }).timeout(15000)
+  }).timeout(25000)
 })
--- test/clientTest.js
+++ test/clientTest.js
@@ -11,7 +11,7 @@ describe('Client', () => {
     const result = await client.listFiles()
     assert.typeOf(result.files, 'array')
     assert.typeOf(result.folders, 'array')
-  }).timeout(10000)
+  }).timeout(25000)
 
   it('upload file', async () => {
     const file = path.join(__dirname, '../images/lion.jpg')
@@ -19,12 +19,12 @@ describe('Client', () => {
     assert(result instanceof Object)
     assert(result.name === 'lion.jpg')
     assert(result.path === '0/lion.jpg')
-  }).timeout(10000)
+  }).timeout(25000)
 
   it('download file', async () => {
     const data = await client.downloadFile('0/birds.jpg')
     assert(data.length === 47259)
-  }).timeout(10000)
+  }).timeout(25000)
 
   it('analyze image', async () => {
     const result = await client.analyzeImage('0/birds.jpg')
@@ -36,7 +36,7 @@ describe('Client', () => {
     const result = await client.aestheticsImage('0/birds.jpg')
     assert(result instanceof Object)
     assert(result.result === 6.076241970062256)
-  }).timeout(10000)
+  }).timeout(25000)
 
   it('remove file', async () => {
     const result = await client.removeFile('0/lion.jpg')
@@ -44,15 +44,15 @@ describe('Client', () => {
     assert(result.file instanceof Object)
     assert(result.file.name === 'lion.jpg')
     assert(result.file.source === '0/lion.jpg')
-  }).timeout(10000)
+  }).timeout(25000)
 
   it('download non existing file', () => {
     client.downloadFile('0/lion.jpg')
       .catch(err => assert(err instanceof Object))
-  }).timeout(10000)
+  }).timeout(25000)
 
   it('analyze non existing image', () => {
     client.analyzeImage('0/lion.jpg')
       .catch(err => assert(err instanceof Object))
-  }).timeout(10000)
+  }).timeout(25000)
 })
