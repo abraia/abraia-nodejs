d88d9478866e5917ab2abcfb65c79d6ecde14184
Jorge Rodríguez-Araújo <jorge@abraiasoftware.com>
2020-01-26 21:39:05 +0100

    Add query utils

--- test/utilsTest.js
+++ test/utilsTest.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert
 
-const { parseString, sizeFormat } = require('../utils')
+const { parseString, sizeFormat, parseQuery, stringifyParams } = require('../utils')
 
 describe('client utils', () => {
   it('parse output', () => {
@@ -11,5 +11,15 @@ describe('client utils', () => {
   it('size format', () => {
     const size = sizeFormat(127806)
     assert(size === '124.81 KB')
+  })
+
+  it('parse query', () => {
+    const params = parseQuery('width=300&height=200')
+    assert(params.width === '300')
+  })
+
+  it('stringify query', () => {
+    const query = stringifyParams({ width: 300, height: 200 })
+    assert(query === 'width=300&height=200')
   })
 })
--- utils.js
+++ utils.js
@@ -14,3 +14,17 @@ module.exports.sizeFormat = (bytes = 0, decimals = 2) => {
   const i = Math.floor(Math.log(bytes) / Math.log(1024))
   return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${sizes[i]}`
 }
+
+module.exports.parseQuery = (query) => {
+  const params = decodeURIComponent(query).split('&').reduce((prev, curr) => {
+    const [name, value] = curr.split('=');
+    prev[name] = value;
+    return prev;
+  }, {});
+  return params;
+};
+
+module.exports.stringifyParams = (params) => {
+  const query = Object.entries(params).map(pair => pair.join('=')).join('&');
+  return encodeURI(query).replace(/#/g, '%23');
+};
